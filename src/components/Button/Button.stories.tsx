d3b115323febdc9db52fea81e6a0775f0eefadc1
grabeqx <grabowski.kontakt@gmail.com>
2020-05-18 17:34:55 +0200

    Storybook knobs + styled Button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, select, text, boolean } from '@storybook/addon-knobs';
 import Button from './Button';
 
 storiesOf('Button', module)
-	.add('Primary', () => <Button>Lorem ipsum</Button>)
-	.add('Primary disabled', () => <Button disabled>Lorem ipsum</Button>);
+	.addDecorator(withKnobs)
+	.add('Primary', () => {
+		const options = {
+			None: null,
+			Primary: 'primary',
+			Secondary: 'secondary',
+			PrimaryNoBg: 'primaryNoBg',
+			SecondaryNoBg: 'secondaryNoBg',
+		};
+
+		const value = select('Color', options, null);
+		const inputText = text('Text', 'Lorem ipsum');
+		const disabled = boolean('Disabled', false);
+		return (
+			<Button
+				secondary={value === 'secondary'}
+				primary={value === 'primary'}
+				primaryNoBg={value === 'primaryNoBg'}
+				secondaryNoBg={value === 'secondaryNoBg'}
+				disabled={disabled}>
+				{inputText}
+			</Button>
+		);
+	});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Button = styled.button`
-	background: ${({ theme }) => theme.primary};
+type ButtonProps = {
+	primary?: boolean;
+	secondary?: boolean;
+	primaryNoBg?: boolean;
+	secondaryNoBg?: boolean;
+};
+
+const Button = styled.button<ButtonProps>`
 	border: none;
 	color: #fff;
 	padding: 5px 20px;
 	border-radius: 4px;
 	font-size: 1.8rem;
-	transition: background 0.2s ease-in-out;
+	transition-property: color, border-color, background;
+	transition-duration: .2s;
+	transition-timing-function: ease-in-out;
 	cursor: pointer;
+	background: ${({ theme }) => theme.primary};
 	&:hover {
 		background: ${({ theme }) => theme.primaryHover};
 	}
@@ -16,6 +25,56 @@ const Button = styled.button`
 		background: ${({ theme }) => theme.primaryDisabled};
 		cursor: default;
 	}
+
+	${({ secondary }) =>
+		secondary &&
+		css`
+			background: ${({ theme }) => theme.secondary};
+			&:hover {
+				background: ${({ theme }) => theme.secondaryHover};
+			}
+			&:disabled {
+				background: ${({ theme }) => theme.secondaryDisabled};
+				cursor: default;
+			}
+		`}
+
+	${({ primaryNoBg }) =>
+		primaryNoBg &&
+		css`
+			background: #fff;
+			color: ${({ theme }) => theme.primary};
+			border: 1px solid ${({ theme }) => theme.primary};
+			&:hover {
+				color: ${({ theme }) => theme.primaryHover};
+				border-color: ${({ theme }) => theme.primaryHover};
+				background: #fff;
+			}
+			&:disabled {
+				color: ${({ theme }) => theme.primaryDisabled};
+				border-color: ${({ theme }) => theme.primaryDisabled};
+				background: #fff;
+				cursor: default;
+			}
+		`}
+	${({ secondaryNoBg }) =>
+		secondaryNoBg &&
+		css`
+			background: #fff;
+			color: ${({ theme }) => theme.secondary};
+			border: 1px solid ${({ theme }) => theme.secondary};
+			&:hover {
+				color: ${({ theme }) => theme.secondaryHover};
+				border-color: ${({ theme }) => theme.secondaryHover};
+				background: #fff;
+			}
+			&:disabled {
+				color: ${({ theme }) => theme.secondaryDisabled};
+				border-color: ${({ theme }) => theme.secondaryDisabled};
+				cursor: default;
+				background: #fff;
+			}
+		`}
 `;
 
 export default Button;
